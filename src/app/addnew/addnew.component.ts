ea291c562888f3c0448a0d64e6d143a9fd5df409
himanshu-skrpkvnp <himanshusinghskrpkvnp@gmail.com>
2024-05-02 23:40:42 +0530

    addnew component changed

--- src/app/addnew/addnew.component.ts
+++ src/app/addnew/addnew.component.ts
@@ -3,6 +3,8 @@ import { Router, RouterLink } from '@angular/router';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+
 
 @Component({
   selector: 'app-addnew',
@@ -10,9 +12,27 @@ import {FormsModule} from '@angular/forms';
   templateUrl: './addnew.component.html',
   styleUrls: ['./addnew.component.css'],
   standalone: true,
-  imports: [ FormsModule , MatFormFieldModule , MatInputModule],
+  imports: [ FormsModule , MatFormFieldModule , MatInputModule , MatProgressBarModule ],
 })
 export class AddnewComponent {
+onProfileChange($event: Event,_t11: any) {
+throw new Error('Method not implemented.');
+}
+onCancel() {
+throw new Error('Method not implemented.');
+}
+onSubmit() {
+throw new Error('Method not implemented.');
+}
+
+  formData: any = {
+    apiname : '',
+    testcase : '',
+    message : '',
+    result : false,
+    profile : false,
+    profilestrategy : ''
+  };
 
   constructor(private router: Router) {
   }
